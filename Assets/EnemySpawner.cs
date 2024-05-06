666ac9586950eb04ef2c2cf2c3d0c5d11722dc7c
Tucker Evans <tuckere13@gmail.com>
2024-05-06 16:30:55 -0400

    Enemy spawner less go

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -2,17 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject characterPrefab;
+    [SerializeField] private float initialSpawnCooldown = 6f;
+    [SerializeField] private float minSpawnCooldown = 1f;
+    [SerializeField] private float cooldownDecreaseRate = 5f;
+    [SerializeField] private float cooldownDecreaseInterval = 60f;
+
+    private float spawnCooldown;
+    private bool isSpawning = false;
+
+    private void Start()
     {
-        
+        spawnCooldown = initialSpawnCooldown;
+        StartCoroutine(DecreaseCooldownRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isSpawning)
+        {
+            isSpawning = true;
+            StartCoroutine(SpawnRoutine());
+        }
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        Spawn();
+        yield return new WaitForSeconds(spawnCooldown);
+        isSpawning = false;
+    }
+
+    private void Spawn()
+    {
+        Vector3 spawnPosition = GetRandomSpawnPosition();
+        Instantiate(characterPrefab, spawnPosition, Quaternion.identity, transform);
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        float xOffset = Random.Range(-5f, 5f);
+        Vector3 spawnPosition = transform.position + new Vector3(xOffset, 0f, 0f);
+        return spawnPosition;
+    }
+
+    private IEnumerator DecreaseCooldownRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cooldownDecreaseInterval);
+            DecreaseCooldown();
+        }
+    }
+
+    private void DecreaseCooldown()
+    {
+        if (spawnCooldown > minSpawnCooldown)
+        {
+            spawnCooldown -= cooldownDecreaseRate;
+            if (spawnCooldown < minSpawnCooldown)
+                spawnCooldown = minSpawnCooldown;
+        }
     }
 }
